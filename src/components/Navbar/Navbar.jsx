d9ee4fa881cb0d5f7da04b07fcb001eaf78f546b
Sreejan <sreejan147@gmail.com>
2022-01-21 19:28:27 +0530

    refactor

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useLocation, useSearchParams } from "react-router-dom";
+import Filters from "../Filters/Filters";
 import { getSearchParams } from "../../utils/getSearchParams";
 import unsplash from "../../assets/unsplash.png";
 import { ReactComponent as SearchIcon } from "../../assets/search.svg";
@@ -112,112 +113,15 @@ const Navbar = ({ showFilters, setShowFilters }) => {
         ) : null}
       </nav>
       {showFilters ? (
-        <div className="filter-container">
-          <form className="sort-by filter-category">
-            <p>SORT BY</p>
-            <br />
-            <input
-              type="radio"
-              id="relevant"
-              name="relevant"
-              value="relevant"
-              checked={isSortBySelected("relevant")}
-              onChange={handleSortByChange}
-            />
-            &nbsp;&nbsp;
-            <label htmlFor="relevant">Relevance</label>
-            <br></br>
-            <input
-              type="radio"
-              id="latest"
-              name="latest"
-              value="latest"
-              checked={isSortBySelected("latest")}
-              onChange={handleSortByChange}
-            />
-            &nbsp;&nbsp;
-            <label htmlFor="latest">Newest</label>
-            <br></br>
-          </form>
-          <form className="color-filter filter-category">
-            <p>COLOR</p>
-            <br />
-            <input
-              type="radio"
-              id="any_color"
-              name="any_color"
-              value="any_color"
-              checked={isColorSelected("any_color")}
-              onChange={handleColorChange}
-            />
-            &nbsp;&nbsp;
-            <label htmlFor="any_color">Any Color</label>
-            <br></br>
-            <input
-              type="radio"
-              id="black_and_white"
-              name="black_and_white"
-              value="black_and_white"
-              checked={isColorSelected("black_and_white")}
-              onChange={handleColorChange}
-            />
-            &nbsp;&nbsp;
-            <label htmlFor="black_and_white">Black and White</label>
-            <br></br>
-          </form>
-          <form className="orientation filter-category">
-            <p>ORIENTATION</p>
-            <br />
-            <input
-              type="radio"
-              id="any"
-              name="any"
-              value="any"
-              checked={isOrientationSelected("any")}
-              onChange={handleOrientationChange}
-            />
-            &nbsp;&nbsp;
-            <label htmlFor="any">Any</label>
-            &nbsp;&nbsp;
-            <input
-              type="radio"
-              id="portrait"
-              name="portrait"
-              value="portrait"
-              checked={isOrientationSelected("portrait")}
-              onChange={handleOrientationChange}
-            />
-            &nbsp;&nbsp;
-            <label htmlFor="portrait">Portrait</label>
-            <br></br>
-            <input
-              type="radio"
-              id="landscape"
-              name="landscape"
-              value="landscape"
-              checked={isOrientationSelected("landscape")}
-              onChange={handleOrientationChange}
-            />
-            &nbsp;&nbsp;
-            <label htmlFor="landscape">Landscape</label>
-            &nbsp;&nbsp;
-            <input
-              type="radio"
-              id="squarish"
-              name="squarish"
-              value="squarish"
-              checked={isOrientationSelected("squarish")}
-              onChange={handleOrientationChange}
-            />
-            &nbsp;&nbsp;
-            <label htmlFor="squarish">Square</label>
-          </form>
-          <div className="clear-filters-container">
-            <button className="clear-filters-btn" onClick={clearFilters}>
-              CLEAR FILTERS
-            </button>
-          </div>
-        </div>
+        <Filters
+          isSortBySelected={isSortBySelected}
+          handleSortByChange={handleSortByChange}
+          isColorSelected={isColorSelected}
+          handleColorChange={handleColorChange}
+          isOrientationSelected={isOrientationSelected}
+          handleOrientationChange={handleOrientationChange}
+          clearFilters={clearFilters}
+        />
       ) : null}
     </div>
   );
